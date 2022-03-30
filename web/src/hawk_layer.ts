0f17a178df63441feaa40772e471f35e6934eada
MatsMacke <61540264+MATSMACKE@users.noreply.github.com>
2022-03-30 16:36:07 +0200

    panic handling

--- web/src/hawk_layer.ts
+++ web/src/hawk_layer.ts
@@ -1,5 +1,5 @@
-
 import init, {Runner} from '@/hawk-wasm/pkg/hawk_wasm'
+import * as hawk_interface from '@/hawk-wasm/hawk_interface'
 
 await init()
 
@@ -9,9 +9,19 @@ let repl_runner = Runner.new()
 
 export function run_code(code: string, in_repl: boolean) {
     if (in_repl) {
-        repl_runner.run(code, true)
+        try {
+            repl_runner.run(code, true)
+        } catch (error: any) {
+            hawk_interface.error(`Uncaught runtime error: ${error.message}`)
+            repl_runner = Runner.new()
+        }
     } else {
-        code_runner.run(code, false)
+        try {
+            code_runner.run(code, false)
+        } catch (error: any) {
+            hawk_interface.error(`Uncaught runtime error: ${error.message}`)
+            code_runner = Runner.new()
+        }
         code_runner.clear()
     }
 }
